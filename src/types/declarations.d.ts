25fbe23b9391b4a74d7f1b06678f4adc9a2a2ef9
Max Bezditko <mb.preply.1@gmail.com>
2024-06-23 23:08:22 +0300

    refactor: remove unused gif declarations

--- src/types/declarations.d.ts
+++ src/types/declarations.d.ts
@@ -13,11 +13,6 @@ declare module '*.jpeg' {
   export default value;
 }
 
-declare module '*.gif' {
-  const value: string;
-  export default value;
-}
-
 declare module '*.svg' {
   const value: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
   export default value;
